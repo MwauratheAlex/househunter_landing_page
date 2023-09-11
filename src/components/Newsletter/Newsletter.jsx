4cd8ce0b3aac5204b604d9f949ef3fe847649db2
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-11 11:01:11 +0300

    Newsletter

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "../../styles/newsletter.css";
+import phone from "../../assets/phone.svg";
 
 export default function Newsletter() {
-  return <section></section>;
+  return (
+    <section className="newsletter container">
+      <div className="newsletter-content">
+        <h1>Subscribe to our Newsletter!</h1>
+        <p>Subscribe to our newsletter and stay updated.</p>
+        <button>Subscribe</button>
+      </div>
+      <img src={phone} />
+    </section>
+  );
 }
