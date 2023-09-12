8c7a5e19ce57a78d5b51048f8f1cab57f0a5f1a0
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-12 20:15:07 +0300

    Responsive HowItWorks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./styles/App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
@@ -20,7 +18,7 @@ function App() {
         <Header />
       </div>
       <main>
-        {/* <HowItWorks /> */}
+        <HowItWorks />
         {/* <FeaturedProperties /> */}
         {/* <Stats /> */}
         {/* <Newsletter /> */}
--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import Card from './Card'
-import "../../styles/how_it_works.css"
-import cartIcon from "../../assets/icon1.svg"
-import toLetIcon from "../../assets/icon2.svg"
-import sellHseIcon from "../../assets/icon3.svg"
+import React from "react";
+import Card from "./Card";
+import "../../styles/how_it_works.css";
+import cartIcon from "../../assets/icon1.svg";
+import toLetIcon from "../../assets/icon2.svg";
+import sellHseIcon from "../../assets/icon3.svg";
 
 export default function HowItWorks() {
   return (
-    <section className='howItWorks'>
-      <h2>How it works?</h2>
-      <p>
-        Everything you need to know when you're looking to buy,
-        rent, or sell - all in one place.
-      </p>
-      <div className='cards'>
-        <Card icon={cartIcon} heading={"Buyer Guides"} cta={"How to buy"}/>
-        <Card icon={toLetIcon} heading={"Renter Guides"} cta={"How to rent"}/>
-        <Card icon={sellHseIcon} heading={"Seller Guides"} cta={"How to sell"}/>
-      </div>
-      <button className='btn'>See Full Guidelines</button>
-    </section>
-  )
+    <div className="container">
+      <section className="howItWorks ">
+        <h2>How it works?</h2>
+        <p>
+          Everything you need to know when you're looking to buy, rent, or sell
+          - all in one place.
+        </p>
+        <div className="cards">
+          <Card icon={cartIcon} heading={"Buyer Guides"} cta={"How to buy"} />
+          <Card
+            icon={toLetIcon}
+            heading={"Renter Guides"}
+            cta={"How to rent"}
+          />
+          <Card
+            icon={sellHseIcon}
+            heading={"Seller Guides"}
+            cta={"How to sell"}
+          />
+        </div>
+        <button className="btn">See Full Guidelines</button>
+      </section>
+    </div>
+  );
 }
